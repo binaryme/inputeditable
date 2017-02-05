63c8521edc58347f82800022d8d7d0207e95a289
nubestra <nubestra@gmail.com>
2017-02-05 01:27:38 -0600

    fix dependencia 'underscore'
    api.use([ 'ecmascript',
                'ui',
                'underscore' ],
                'client' );

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nubestra:inputeditable',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Editable input with autosave for meteor',
   // URL to the Git repository containing the source code for this package.
